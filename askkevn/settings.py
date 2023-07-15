c93690c835870c7ae4b2eb97e9964d8b982f4bf6
loopcircle <loopcapinc@gmail.com>
2023-07-14 23:48:38 -0400

    update WN2

--- askkevn/settings.py
+++ askkevn/settings.py
@@ -137,7 +137,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.StaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
